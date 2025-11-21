331fdfcfd2675a2e9d8a4d672075e82f0f702354
AVSuni <anttsuni@amd.com>
2025-11-21 10:34:42 +0200

    Add basic scaffolding for the project (#1)
    * Add basic scaffolding for the project
    
    * add missing copyright-template
    
    * rename ci runs for branch ruleset
    
    * add code owners
    
    * PR review changes

---
+++ api/v1alpha1/aimclusterruntimeconfig_types.go
@@ -0,0 +1,100 @@
+/*
+MIT License
+
+Copyright (c) 2025 Advanced Micro Devices, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AIMClusterRuntimeConfigSpec defines the desired state of AIMClusterRuntimeConfig
+type AIMClusterRuntimeConfigSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// The following markers will use OpenAPI v3 schema to validate the value
+	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
+
+	// foo is an example field of AIMClusterRuntimeConfig. Edit aimclusterruntimeconfig_types.go to remove/update
+	// +optional
+	Foo *string `json:"foo,omitempty"`
+}
+
+// AIMClusterRuntimeConfigStatus defines the observed state of AIMClusterRuntimeConfig.
+type AIMClusterRuntimeConfigStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// For Kubernetes API conventions, see:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	// conditions represent the current state of the AIMClusterRuntimeConfig resource.
+	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
+	//
+	// Standard condition types include:
+	// - "Available": the resource is fully functional
+	// - "Progressing": the resource is being created or updated
+	// - "Degraded": the resource failed to reach or maintain its desired state
+	//
+	// The status of each condition is one of True, False, or Unknown.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// AIMClusterRuntimeConfig is the Schema for the aimclusterruntimeconfigs API
+type AIMClusterRuntimeConfig struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of AIMClusterRuntimeConfig
+	// +required
+	Spec AIMClusterRuntimeConfigSpec `json:"spec"`
+
+	// status defines the observed state of AIMClusterRuntimeConfig
+	// +optional
+	Status AIMClusterRuntimeConfigStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// AIMClusterRuntimeConfigList contains a list of AIMClusterRuntimeConfig
+type AIMClusterRuntimeConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []AIMClusterRuntimeConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&AIMClusterRuntimeConfig{}, &AIMClusterRuntimeConfigList{})
+}
