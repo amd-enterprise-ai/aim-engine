047b980b966318ed99957b934e50791fb3cae58a
Alex Saliniemi <alex.saliniemi@amd.com>
2025-12-01 18:44:07 +0000

    test: add domain controller unit tests

--- internal/aimmodelcache/reconcile_test.go
+++ internal/aimmodelcache/reconcile_test.go
@@ -13,7 +13,7 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -121,7 +121,6 @@ func TestObserve(t *testing.T) {
 
 	reconciler := &Reconciler{}
 	obs, err := reconciler.Observe(context.Background(), cache, fetchResult)
-
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
