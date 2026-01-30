8915acace471fdb2acbb949446a595e633bb2276
bjorn-amd <bjorn.ahlstrom@amd.com>
2026-01-30 11:39:52 +0100

    Fix chainsaw test for allocatedSize and cosmetic kubebuild marker for rbac

--- internal/controller/aimmodelcache_controller.go
+++ internal/controller/aimmodelcache_controller.go
@@ -78,7 +78,7 @@ type AIMModelCacheReconciler struct {
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 // +kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=create;get;list;watch;patch;update
-//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,resourceNames=modelcache-status-updater,verbs=bind
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,resourceNames=modelcache-status-updater,verbs=bind
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
